92c62edfb9b057ca4b686d4a3549a528ec604dfb
TiagoWurmli <ti.wurmli@hotmail.com>
2023-11-21 17:00:33 -0300

    Starting while stuff

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        int response = 0;
+        while (response != 5)
+        {
+            Console.Write("Please select one of the following choice: \n1.Write \n2.Display \n3.Load \n4.Save \n5.Quit \nWhat would you like to do? ");
+            response = int.Parse(Console.ReadLine());
+
+            if (response == 1)
+        }
+
         Journal journal = new Journal();
 
         PromptGenerator prompt = new PromptGenerator();
@@ -22,11 +32,22 @@ class Program
         entry._entryText = "I wrote this";
         journal.AddEntry(entry);
         //entry.Display();
+        Entry entry2 = new Entry();
         DateTime theCurrentTimes = DateTime.Now;
-        entry._date = theCurrentTimes.ToShortDateString();
+        entry2._date = theCurrentTimes.ToShortDateString();
+        entry2._promptText = prompt.GetRamomPrompt();
+        entry2._entryText = "I wrote this again";
+        journal.AddEntry(entry2);
+        journal.DisplayAll();
+
+        journal.SaveToFile("firstJournal.txt");
+
+        entry._date = theCurrentTime.ToShortDateString();
         entry._promptText = prompt.GetRamomPrompt();
-        entry._entryText = "I wrote this again";
-        journal.AddEntry(entry);
+        entry._entryText = "I wrote this so lets be it";
+        journal.DisplayAll();
+
+        journal.LoadFromFile("firstJournal.txt");
         journal.DisplayAll();
 
     }
