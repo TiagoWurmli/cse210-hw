7c3125c28faa1ab34d0fc345cc50a6b578fbae34
TiagoWurmli <ti.wurmli@hotmail.com>
2023-11-21 17:22:35 -0300

    Only need to correct save and load file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,16 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        int response = 0;
-        while (response != 5)
-        {
-            Console.Write("Please select one of the following choice: \n1.Write \n2.Display \n3.Load \n4.Save \n5.Quit \nWhat would you like to do? ");
-            response = int.Parse(Console.ReadLine());
-
-            if (response == 1)
-        }
-
         Journal journal = new Journal();
 
         PromptGenerator prompt = new PromptGenerator();
@@ -23,32 +13,48 @@ class Program
         prompt._prompts.Add("What was the strongest emotion I felt today?");
         prompt._prompts.Add("If I had one thing I could do over today, what would it be?");
 
-        //Console.WriteLine(prompt.GetRamomPrompt());
+        int response = 0;
+        while (response != 5)
+        {
+            Console.Write("Please select one of the following choice: \n1.Write \n2.Display \n3.Load \n4.Save \n5.Quit \nWhat would you like to do? ");
+            response = int.Parse(Console.ReadLine());
 
-        Entry entry = new Entry();
-        DateTime theCurrentTime = DateTime.Now;
-        entry._date = theCurrentTime.ToShortDateString();
-        entry._promptText = prompt.GetRamomPrompt();
-        entry._entryText = "I wrote this";
-        journal.AddEntry(entry);
-        //entry.Display();
-        Entry entry2 = new Entry();
-        DateTime theCurrentTimes = DateTime.Now;
-        entry2._date = theCurrentTimes.ToShortDateString();
-        entry2._promptText = prompt.GetRamomPrompt();
-        entry2._entryText = "I wrote this again";
-        journal.AddEntry(entry2);
-        journal.DisplayAll();
+            if (response == 1) {
+                Entry entry = new Entry();
 
-        journal.SaveToFile("firstJournal.txt");
+                DateTime theCurrentTime = DateTime.Now;
+                entry._date = theCurrentTime.ToShortDateString();
 
-        entry._date = theCurrentTime.ToShortDateString();
-        entry._promptText = prompt.GetRamomPrompt();
-        entry._entryText = "I wrote this so lets be it";
-        journal.DisplayAll();
+                entry._promptText = prompt.GetRamomPrompt();
+                Console.WriteLine(entry._promptText);
+                entry._entryText = Console.ReadLine();
 
-        journal.LoadFromFile("firstJournal.txt");
-        journal.DisplayAll();
+                journal.AddEntry(entry);
+            }
 
+            else if (response == 2) {
+                journal.DisplayAll();
+            }
+
+            else if (response == 3) {
+                Console.WriteLine("What is the filename?");
+                string journalFile = Console.ReadLine();
+                journal.LoadFromFile(journalFile);
+            }
+
+            else if (response == 4) {
+                Console.WriteLine("What is the filename?");
+                string journalFile = Console.ReadLine();
+                journal.SaveToFile(journalFile);
+            }
+
+            else if (response ==5) {
+                Console.WriteLine("Goodbye!");
+            }
+
+            else {
+                Console.WriteLine("Please select a valid value.");
+            }
+        }
     }
 }
