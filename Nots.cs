a1deaacd8ca6a8e1e85cc9f616922f02d1666a14
TiagoWurmli <ti.wurmli@hotmail.com>
2023-11-22 15:08:26 -0300

    Did Learning 03

--- Nots.cs
+++ Nots.cs
@@ -191,3 +191,32 @@ Instanciar // Um verbo que significa "criar uma instância de".
 Ex: //Podemos instanciar a classe Person para criar um novo objeto.
 Método // Uma função de membro. Os métodos são chamados usando "notação de ponto" com uma instância da classe antes do ponto. 
 Ex: //pessoa1.GetEasternName()
+
+!Constructors! (construtores)
+Person p = new Person(); // This calls a no-argument constructor and doesn't need any information
+
+Ex: public class Person
+{
+    private string _title;
+    private string _firstName;
+    private string _lastName;
+    public Person() {
+        _title = "";
+        _firstName = "Anonymous";
+        _lastName = "Unknown";
+    }
+    public Person(string first, string last){
+        _title = "";
+        _firstName = first;
+        _lastName = last;
+    }
+    public Person(string title, string first, string last){
+        _title = title;
+        _firstName = first;
+        _lastName = last;
+    }
+Person p1 = new Person(); // Don't pass any information to get the default values
+Person p2 = new Person("Jane", "Doe"); // pass the first and last names
+Person p3 = new Person("Mrs.", "Jane", "Doe"); // pass all three variables
+
+
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        fraction.Fractoin();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        
+        Fraction fraction2 = new Fraction();
+        fraction2.Fractoin(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction();
+        fraction3.Fractoin(3,4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction();
+        fraction4.Fractoin(1,3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
     }
 }
