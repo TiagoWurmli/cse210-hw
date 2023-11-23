73dbde6ba9707dfe916b9350a1636bd3ad139c9c
TiagoWurmli <ti.wurmli@hotmail.com>
2023-11-23 20:58:49 -0300

    Skeleton Finished

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,9 @@
+public class BreathingActivity : Activity {
+
+    public BreathingActivity(){
+
+    }
+    public void Run(){
+
+    }
+}
---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,19 @@
+public class ListingngActivity : Activity {
+
+    private int _count;
+    public List<string> _prompts = new List<string>();
+
+    public ListingngActivity(){
+
+    }
+    public void Run(){
+
+    }
+    public string GetRandomPrompt(){
+        return "";
+    }
+    public List<string> GetListFromUser(){
+        return _prompts;
+    }
+
+}
