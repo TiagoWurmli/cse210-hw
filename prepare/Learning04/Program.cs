7e6438e6ebb44e52011cd760047d78cb1a2d4f83
TiagoWurmli <ti.wurmli@hotmail.com>
2023-11-23 19:35:42 -0300

    Finish Prepre 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,9 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
 
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomehorkList());
 
+        WritingAssignment writingAssignment  = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
         
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformtion());
     }
 }
