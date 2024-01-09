4174b84055fbf300b8cf2c7d819a103ef87d67ab
TiagoWurmli <ti.wurmli@hotmail.com>
2024-01-09 16:41:32 -0300

    Mede prepare

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Reference reference = new Reference("Proverbs",3,5,6);
-        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thune heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
 
         bool isCompletelyHidden = scripture.IsCompletelyHidden();
 
