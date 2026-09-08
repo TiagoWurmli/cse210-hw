bc275a31234006a1c482d2e88740bf6fb4580f5e
Tiago Ismael Wurmli <twurmli@byupathway.edu>
2026-09-08 16:57:23 -0300

    Did week1 ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgian = "";
+        do
+        {
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+            int guessNumber = 0;
+            int attempts = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                attempts ++;
+
+                if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it! (Guess attempts: {attempts})");
+                }
+                else if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }  
+            } while (magicNumber != guessNumber);
+
+            Console.Write("Do you want to play again? ");
+            playAgian = Console.ReadLine();
+
+        } while (playAgian == "yes");
     }
 }
